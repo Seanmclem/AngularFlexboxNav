9159d923c77b898e84a4478cf9d171ec382b1a71
Seankps <seanmclements@gmail.com>
2018-06-12 22:56:11 -0400

    fixed self-referencing selector, updated readme

--- nav/nav.component.ts
+++ nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'nav',
+  selector: 'navigation',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
